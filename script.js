1a0a6b564490bc19d13f520f1be3c6eb71d94fca
bbbozhena <bbbozhenka@gmail.com>
2022-06-15 17:50:39 +0300

    add js

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const oneBlock = document.querySelector('.one-block');
 const twoBlock = document.querySelector('.two-block');
 
-oneBlock.addEventListener('click', function onClick (event) {
-    oneBlock.style.backgroundColor = 'blue';
 
-})
+
+ oneBlock.addEventListener('click', (e) => {
+
+    if (oneBlock.style.backgroundColor == 'green') {
+        oneBlock.style.backgroundColor = 'blue';
+        twoBlock.style.backgroundColor = 'green';
+    } else {
+        oneBlock.style.backgroundColor = 'green';
+        twoBlock.style.backgroundColor = 'blue';
+    
+    }
+    
+    
+ });
+
+
+ twoBlock.addEventListener('click', (e) => {
+     if (twoBlock.style.backgroundColor == 'blue') {
+        oneBlock.style.backgroundColor = 'blue';
+        twoBlock.style.backgroundColor = 'green';
+     } else {
+        oneBlock.style.backgroundColor = 'green';
+        twoBlock.style.backgroundColor = 'blue';
+     }
+    
+    
+ });
+
+
+
+
+
+
+
