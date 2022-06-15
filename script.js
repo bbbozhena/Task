d7dcb4f7dd505dd6d1eb58590555900202aa5ca5
bbbozhena <bbbozhenka@gmail.com>
2022-06-15 16:39:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const oneBlock = document.querySelector('.one-block');
+const twoBlock = document.querySelector('.two-block');
+
+oneBlock.addEventListener('click', function onClick (event) {
+    oneBlock.style.backgroundColor = 'blue';
+
+})
