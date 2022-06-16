46856a290f23afc2692020d75435dd12358b02f9
bbbozhena <bbbozhenka@gmail.com>
2022-06-16 19:32:56 +0300

    ref

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
 const oneBlock = document.querySelector('.one-block');
 const twoBlock = document.querySelector('.two-block');
 
+oneBlock.addEventListener('click', (e) => changeColor (e, 'green'));
+twoBlock.addEventListener('click', (e) => changeColor (e, 'blue'));
 
 
- oneBlock.addEventListener('click', (e) => {
+function changeColor (e, color) {
+   switch (color) {
+       case 'green':
+           oneBlock.style.backgroundColor = 'blue';
+           twoBlock.style.backgroundColor = 'green';
+           break
+           case 'blue':
+            oneBlock.style.backgroundColor = 'green';
+            twoBlock.style.backgroundColor = 'blue';
+            break
+            default: ''
 
-    if (oneBlock.style.backgroundColor == 'green') {
-        oneBlock.style.backgroundColor = 'blue';
-        twoBlock.style.backgroundColor = 'green';
-    } else {
-        oneBlock.style.backgroundColor = 'green';
-        twoBlock.style.backgroundColor = 'blue';
-    
-    }
-    
-    
- });
+   }
+}
 
 
- twoBlock.addEventListener('click', (e) => {
-     if (twoBlock.style.backgroundColor == 'blue') {
-        oneBlock.style.backgroundColor = 'blue';
-        twoBlock.style.backgroundColor = 'green';
-     } else {
-        oneBlock.style.backgroundColor = 'green';
-        twoBlock.style.backgroundColor = 'blue';
-     }
-    
-    
- });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (oneBlock.style.backgroundColor == 'green') {
+//     oneBlock.style.backgroundColor = 'blue';
+//     twoBlock.style.backgroundColor = 'green';
+// } else {
+//     oneBlock.style.backgroundColor = 'green';
+//     twoBlock.style.backgroundColor = 'blue';
+
+// }
+
+// if (twoBlock.style.backgroundColor == 'blue') {
+//     oneBlock.style.backgroundColor = 'blue';
+//     twoBlock.style.backgroundColor = 'green';
+//  } else {
+//     oneBlock.style.backgroundColor = 'green';
+//     twoBlock.style.backgroundColor = 'blue';
+//  }
+
+
+
+
+
+
 
 
 
