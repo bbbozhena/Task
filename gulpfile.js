4f6c52be1ba0e7473f02324724392822c4385cc0
bbbozhena <bbbozhenka@gmail.com>
2022-06-23 21:02:28 +0300

    gulp scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 
-require('gulp-cli')();
+var gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+gulp.task("sass", function () {
+  return (
+    gulp
+      .src("style.scss")
+      .pipe(sass())
+      .on("error", sass.logError)
+      .pipe(gulp.dest("styles"))
+  );
+});
 
 
-var gulp = require ('gulp');
 
-var sass = require ('gulp-sass');
 
-gulp.task('sass', function(){
-    return gulp.src ('style.scss')
-      .pipe(sass()) 
-      .pipe(gulp.dest('style.css'))
-  });
